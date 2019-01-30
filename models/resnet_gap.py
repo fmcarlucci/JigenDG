f9b6d4b0ad2d4b9c5ccd41374a9970789f642b54
enoonIT <enoonIT@users.noreply.github.com>
2019-01-30 10:26:35 +0000

    Create resnet_gap.py

---
+++ models/resnet_gap.py
@@ -0,0 +1,78 @@
+from torch import nn
+from torch.utils import model_zoo
+from torchvision.models.resnet import BasicBlock, model_urls, Bottleneck
+import torch
+
+class ResNetGAP(nn.Module):
+    def __init__(self, block, layers, jigsaw_classes=1000, classes=100, domains=3):
+        self.inplanes = 64
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.jigsaw_classifier = nn.Linear(512 * block.expansion, jigsaw_classes)
+        self.class_classifier = nn.Conv2d(512, classes, kernel_size=1,padding=0)
+        #self.domain_classifier = nn.Linear(512 * block.expansion, domains)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def is_patch_based(self):
+        return False
+
+    def forward(self, x, **kwargs):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        cl = self.class_classifier(x)
+        
+        x = self.avgpool(x)
+        cl = self.avgpool(cl)
+        x = x.view(x.size(0), -1)
+        return self.jigsaw_classifier(x), cl.view(cl.shape[0], -1)
+
+
+def resnet18GAP(pretrained=True, **kwargs):
+    """Constructs a ResNet-18 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNetGAP(BasicBlock, [2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']), strict=False)
+    return model
